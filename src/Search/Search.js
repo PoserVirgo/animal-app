dfca675a5db7da0a6e5a16873d4fceb05321525d
PoserVirgo <isalm.faisalmuhamad@gmail.com>
2023-11-29 22:54:28 +0700

    update

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -3,18 +3,43 @@ import axios from "axios";
 import "./Search.css";
 import { NavLink } from "react-router-dom";
 
-const Search = () => {
+const Search = (props) => {
   const [data, setData] = useState([]);
   const [name, setName] = useState("");
   const [isLoading] = useState(false);
   const [isError] = useState(false);
+  
+  const [dataItem, setDataItem] = useState([]);
+  const [isLoadingItem, setIsLoadingItem] = useState(false);
+  const [isErrorItem, setIsErrorItem] = useState(false);
+  const { history } = props;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsErrorItem(false);
+      setIsLoadingItem(true);
+
+      try {
+        const results = await axios(
+          "https://my-json-server.typicode.com/PoserVirgo/animal-app/db"
+        );
+        setDataItem(results.data.exopet)
+      } catch (err) {
+        setIsErrorItem(true);
+        setTimeout(() => setIsErrorItem(false), 4000);
+      }
+      setIsLoadingItem(false);
+    };
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const doSearch = (e) => {
       axios
         .get(
           "https://my-json-server.typicode.com/PoserVirgo/animal-app/exopet?common_name=" +
-            name
+          name
         )
         .then((results) => {
           setData(results.data);
@@ -23,6 +48,28 @@ const Search = () => {
     doSearch();
   }, [name]);
   console.log(data);
+
+  const renderListItem = () => {
+    return (
+      <div className="item-container">
+        {dataItem.map((item, index) => {
+          return (
+            <div class="box" key={index}>
+              <NavLink
+                to={`/Cardsdetails/${item.id}`}
+                onClick={() =>
+                  history.push(`/Cardsdetails/${item.id}`)
+                }
+              >
+                <img class="img-list" src={item.image} alt="" />
+                <p class="name">{item.common_name}</p>
+              </NavLink>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
 
   const renderList = () => {
     return (
@@ -65,6 +112,19 @@ const Search = () => {
     }
   };
 
+  const renderErrorItem = () => {
+    if (isErrorItem) {
+      return (
+        <div
+          className="alert alert-warning alert-dismissible fade show"
+          role="alert"
+        >
+          Unable to Get data, Please try again next time
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="search-container">
       {renderError()}
@@ -75,6 +135,12 @@ const Search = () => {
         <div className="search-loading">Loading...</div>
       ) : (
         <div className="search-result">{renderList()}</div>
+      )}
+      {renderErrorItem()}
+      {isLoadingItem ? (
+        <div className="loading-state">Loading...</div>
+      ) : (
+        <div className="render">{renderListItem()}</div>
       )}
     </div>
   );
